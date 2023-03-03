e17002bb06d072540a146ae1d45b6a7963e6eb73
lizsjh <37032480+lizsjh@users.noreply.github.com>
2023-03-03 16:03:06 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,100 @@
+var botui=new BotUI('app');
+const response=new Array();
+
+botui.message.add({
+    delay:1500,
+    loading: true,
+    content: 'Hello. This is Alex, and I am a bot created by the customer service department.'
+}).then(function(){
+    return botui.message.add({
+        delay:2000,
+        loading: true,
+        content:'I will handle your request today. What brings you here?'
+    });
+}).then(function(){
+    return botui.action.text({
+        action: {
+          placeholder: 'Enter your message.'
+        }
+    
+    });
+}).then(function (res) { 
+    console.log(res.value);
+    response.push(res.value);
+}).then(function(){
+    return botui.message.add({
+        delay:3000,
+        loading: true,
+        content:'I can tackle that issue. First, could you tell me why you need to replace or return this textbook?'
+    });
+}).then(function(){
+    return botui.action.text({
+        action: {
+          placeholder: 'Enter your message.'
+        }
+    
+    });
+}).then(function (res) { 
+    console.log(res.value);
+    response.push(res.value);
+}).then(function(){
+    return botui.message.add({
+        delay:2000,
+        loading: true,
+        content:'Got it. Could you input your order number below?'
+    });
+}).then(function(){
+    return botui.action.text({
+        action: {
+          placeholder: 'Enter your message.'
+        }
+    
+    });
+}).then(function (res) { 
+    console.log(res.value);
+    response.push(res.value);
+}).then(function(){
+    return botui.message.add({
+        delay:2000,
+        loading: true,
+        content:'Alright. I am retrieving your order. Please wait for a moment.'
+    });
+}).then(function(){
+    return botui.message.add({
+        delay:5000,
+        loading: true,
+        content:'The 3rd edition is currently in stock. For your information, you need to pay $50 more for the newer edition. Would you still like to exchange the book?'
+    });
+}).then(function(){
+    return botui.action.text({
+        action: {
+          placeholder: 'Enter your message.'
+        }
+        });
+}).then(function (res) { 
+    console.log(res.value);
+    response.push(res.value);
+}).then(function(){
+    return botui.message.add({
+        delay:3000,
+        loading: true,
+        content:'I am processing your request. Please hold on.'
+    });
+}).then(function(){
+    return botui.message.add({
+        delay:6000,
+        loading: true,
+        content:'I have processed your request, and the 3rd edition will be delivered in two days.'
+    });
+}).then(function(){
+    sendcomplete();
+    return botui.message.add({
+        delay:4000,
+        loading: true,
+        content:'I have resolved the issue. Please contact us again if you need further assistance. Bye.'
+    });
+});
+
+function sendcomplete(){
+    window.parent.postMessage({"message": "completed","text1":response[0],"text2":response[1],"text3":response[2], "text4":response[3]}, "*");
+};
